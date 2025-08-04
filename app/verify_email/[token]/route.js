4528cec1f27673a5250f7854e37ae1956550e0c7
Arvind Kumar <ak8188860205@gmail.com>
2025-08-05 01:46:10 +0530

    third  connment

--- app/verify_email/[token]/route.js
+++ app/verify_email/[token]/route.js
@@ -1,3 +1,5 @@
+"use client";
+
 import { NextResponse } from "next/server";
 import { jwtVerify } from "jose";
 import { connectDB } from "@/lib/databaseConnection";
--- app/verify_email/success/page.js
+++ app/verify_email/success/page.js
@@ -1,3 +1,5 @@
+"use client";
+
 export default function SuccessPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 px-4">
